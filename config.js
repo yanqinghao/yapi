248ef87c2d6411c2539fd64f9cb08baa026d6359
YanQinghao <woshiyanqinghao@gmail.com>
2020-04-10 17:58:35 +0800

    fix

--- config.js
+++ config.js
@@ -1,15 +1,11 @@
 module.exports = {
-  "port": "3000",
+  "port": process.env.connectString,
   "adminAccount": "admin@admin.com",
   "timeout":120000,
   "db": {
     "connecString": process.env.connectString,
-    "servername": "127.0.0.1",
-    "DATABASE": "yapi",
-    "port": process.env.PORT,
     "user": process.env.connectUser,
     "pass": process.env.connectPass,
-    "authSource": "yapi"
   },
   "mail": {
     "enable": true,
